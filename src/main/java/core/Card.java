50084661c5fc141881db4107e3950cc699264a47
JustinM250 <25014394@saultcollege.ca>
2026-02-18 12:43:30 -0500

    IDE works?

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,33 +1,29 @@
-/*
-    NOTE:
 
-    Add to this type any variables and/or methods required
-    to represent one playing card.
-
-    You MAY change this to a record/enum as you see fit.
- */
 package core;
 
 import java.util.Random;
 public class Card {
-    private enum Rank{
+    static public enum Rank{
         ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
     }
-    private enum Suit{
+    static public enum Suit{
         HEART, DIAMOND, CLUB, SPADE;
     }
 
-    private static final Random randomGenerator = new Random();
-    private static Rank getRandomRank(){
-        return Rank.values()[randomGenerator.nextInt(Rank.values().length)];
-    }
-    private static Suit getRandomSuit(){
-        return Suit.values()[randomGenerator.nextInt(Suit.values().length)];
-    }
+    //    private static final Random randomGenerator = new Random();
+//    private static Rank getRandomRank(){
+//        return Rank.values()[randomGenerator.nextInt(Rank.values().length)];
+//    }
+//    private static Suit getRandomSuit(){
+//        return Suit.values()[randomGenerator.nextInt(Suit.values().length)];
+//    }
     Rank cardRank;
     Suit cardSuit;
 
-    private int rankToInt(){
+    /**
+     * @return returns the integer equivalent of the card's rank. ACE = 1, TWO-TEN are self explanatory. JACK = 11, QUEEN = 12, KING = 13.
+     */
+    public int rankToInt(){
         switch(cardRank){
             case ACE: return 1;
             case TWO: return 2;
@@ -46,7 +42,10 @@ public class Card {
         return -1;
     }
 
-    private String suitToString(){
+    /**
+     * @return returns the string equivalent of the suit.
+     */
+    public String suitToString(){
         switch(cardSuit){
             case HEART: return "♥";
             case DIAMOND: return "♦";
@@ -55,15 +54,33 @@ public class Card {
         }
         return "";
     }
-    public Card(){
-        this.cardRank = getRandomRank();
-        this.cardSuit = getRandomSuit();
+//    public Card(){
+//        this.cardRank = getRandomRank();
+//        this.cardSuit = getRandomSuit();
+//    }
+
+    public Card(Rank r, Suit s){
+        this.cardRank = r;
+        this.cardSuit = s;
     }
 
+    /**
+     * @return returns the rank and suit, in a string
+     */
     public String cardToString(){
         return rankToInt() + suitToString();
     }
 
-
-
+    // This doesn't work with my IDE issues.
+//    public boolean equals(Object object) {
+//        if (object == null || getClass() != object.getClass()) return false;
+//        if (!super.equals(object)) return false;
+//        Card card = (Card) object;
+//        return cardRank == card.cardRank && cardSuit == card.cardSuit;
+//    }
+//
+//    public int hashCode() {
+//        return Objects.hash(super.hashCode(), cardRank, cardSuit);
+//    }
 }
+// END ------------------------------------------------------------------------------- 

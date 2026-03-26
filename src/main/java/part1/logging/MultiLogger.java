34d7905f3630132a9e20c6dea1f954eddd9dd5bb
JustinM250 <25014394@saultcollege.ca>
2026-03-26 15:13:11 -0400

    3:12,Mar26

--- src/main/java/part1/logging/MultiLogger.java
+++ src/main/java/part1/logging/MultiLogger.java
@@ -6,12 +6,13 @@ When the ‘log’ method is called on MultiLogger it calls the log method on AL
 (I.e., with MultiLogger, you can send log messages to multiple loggers, each which does logging in its own way!)
 */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultiLogger implements Logger {
 
-    List<Logger> listOfLoggers;
-    public MultiLogger(List<Logger> newListOfLoggers ){
+    ArrayList<Logger> listOfLoggers = new ArrayList<Logger>();
+    public MultiLogger(ArrayList<Logger> newListOfLoggers ){
         for( Logger l : newListOfLoggers ){
             listOfLoggers.add(l);
         }
@@ -20,7 +21,7 @@ public class MultiLogger implements Logger {
 
     public void log(String message, LogLevel level) {
         for (Logger l: listOfLoggers){
-            l.log(l. );
+            l.log(message, level);
         }
     }
 }

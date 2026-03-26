c029a47b05b919c8463b0fbbf3d1ee4ffef482d9
JustinM250 <25014394@saultcollege.ca>
2026-03-26 15:29:41 -0400

    3:29,Mar26

--- src/main/java/part2/Main.java
+++ src/main/java/part2/Main.java
@@ -11,6 +11,8 @@ public class Main {
     static void main(String[] args) {
         var bigMacs = loadData();
 
+        // 3:29, time to finish part 2
+
         // TODO: Find the BigMac entry for Canada in the year 2022
 
         // TODO: Create a list containing only the data for Canada
@@ -26,17 +28,26 @@ public class Main {
 
     }
 
+//    public static List<BigMac> loadData() {
+//        try(var lines = Files.lines(Path.of("BigMacPrices.csv"))) {
+//            return lines.map(
+//                    // TODO: replace this ENTIRE lambda expression with a function reference that parses the line from the file
+//                    //       (see the parseCsvLine method below)
+//                    line -> {
+//
+//                        return new BigMac(1, "CAD", "ca", 1.0, 1.0, 1.0);
+//                    })
+//                    .toList();
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
     public static List<BigMac> loadData() {
         try(var lines = Files.lines(Path.of("BigMacPrices.csv"))) {
-            return lines.map(
-                    // TODO: replace this ENTIRE lambda expression with a function reference that parses the line from the file
-                    //       (see the parseCsvLine method below)
-                    line -> {
-
-                        return new BigMac(1, "CAD", "ca", 1.0, 1.0, 1.0);
-                    })
-                    .toList();
-
+//            return lines.map(line -> {return parseCsvLine(line);} ).toList();
+            return lines.map(Main::parseCsvLine).toList();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
